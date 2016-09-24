7462f97629819cf1b00468275b20870da59a8429
Sai kishore Amirishetty <sai.kishi2010@gmail.com>
2016-09-24 20:55:22 +0530

    assignment1_3

---
+++ src/Assignment3.java
@@ -0,0 +1,21 @@
+
+public class Assignment3 {
+public static void main(String[] args) {
+	int a=10;
+	int b=20;
+	int c;
+	System.out.println("Before SWAP");
+	System.out.println("a:"+a);
+	System.out.println("b:"+b);
+	a = a+b;
+	b=a-b;
+	a=a-b;	
+	System.out.println("After SWAP");
+	System.out.println("a:"+a);
+	System.out.println("b:"+b);
+	
+	
+}
+	
+	
+}
